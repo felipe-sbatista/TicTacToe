ae78a0d29b5afac7d07c34b9516f9926b392d6ad
Mariana <quirino193@gmail.com>
2020-04-21 16:23:35 -0300

    Translate Circle

--- drawer.py
+++ drawer.py
@@ -3,6 +3,12 @@ from OpenGL.GL import *
 import math
 import env
 
+posSquare = 0.33
+posCircle = posSquare * 2
+
+tX = [-posCircle, 0, posCircle]
+tY = [posCircle, 0, -posCircle]
+
 class Drawer():
     def draw_grid(self):
         # GL_LINES pega de dois em dois
@@ -11,45 +17,47 @@ class Drawer():
         # Vertical
 
         # Primeira coluna
-        glVertex2f(.33, 1)
-        glVertex2f(.33, -1)
+        glVertex2f(posSquare, 1)
+        glVertex2f(posSquare, -1)
 
         # Segunda coluna
-        glVertex2f(-.33, 1)
-        glVertex2f(-.33, -1)
+        glVertex2f(-posSquare, 1)
+        glVertex2f(-posSquare, -1)
 
         # Horizontal
 
         # Primeira linha
-        glVertex2f(1, .33)
-        glVertex2f(-1, .33)
+        glVertex2f(1, posSquare)
+        glVertex2f(-1, posSquare)
 
         # Segunda linha
-        glVertex2f(1, -.33)
-        glVertex2f(-1, -.33)
+        glVertex2f(1, -posSquare)
+        glVertex2f(-1, -posSquare)
 
         glEnd()
         glFlush()
 
-    def draw_circle(self, radius):
-        # LINE LOOP pega linhas interligadas, no caso nosso circulo
+    def draw_circle(self, posX, posY):
         glLineWidth(6)
+        glPushMatrix()
+
+        glMatrixMode(GL_MODELVIEW)
+        glTranslatef(tX[posX], tY[posY], 0)
+
+        # LINE LOOP pega linhas interligadas, no caso nosso circulo
         glBegin(GL_LINE_LOOP)
 
-        deg = 3.1415/180
+        deg = 3.1415 / 180
         radius = -.2
         for i in range(360):
-            rad = deg*i
-            glVertex2f(math.cos(rad)*radius,math.sin(rad)*radius)
-
-        #aqui da ruim
-        glMatrixMode(GL_MODELVIEW)
-        glTranslatef(2, 0, 0)
+            rad = deg * i
+            glVertex2f((math.cos(rad) * radius), math.sin(rad) * radius)
 
         glEnd()
         glFlush()
+
+        glPopMatrix()
 
     # position contem o quadrante que vamos marcar
     def draw_x(self, position):
         pass
-
--- game.py
+++ game.py
@@ -20,5 +20,5 @@ class Game(QOpenGLWidget):
 
         drawer = Drawer()
         drawer.draw_grid()
-        drawer.draw_circle(0)
+        drawer.draw_circle(1, 1)
 
